ef54d50dd09d08de17f3ac4738e3a32d473f2615
gzovkic <g.zovkic@web.de>
2026-08-04 11:07:29 +0200

    FIX(main.cpp): switched digitalwrite() statement

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include <Arduino.h>
 
+int water_pump = 2;
+int cap_soil_sen = 0;
+
 void  setup()  {
   Serial.begin(9600);
+  pinMode(water_pump, OUTPUT);
 }
 
 void loop() {
   int val;
-  val = analogRead(0);
+  val = analogRead(cap_soil_sen);
   Serial.println(val);
-  delay(100);
+  if(val < 400)
+  {
+    digitalWrite(water_pump, HIGH);
+  }
+  else
+  {
+    digitalWrite(water_pump, LOW);
+  }
 }
